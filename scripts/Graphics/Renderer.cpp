0c2cb92928f164b87b335bd5a73a643a55855f15
Shivansh Ahuja <kartikaye02@gmail.com>
2024-05-26 12:59:09 +0530

    Added Window

---
+++ scripts/Graphics/Renderer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by karti on 25-05-2024.
+//
+
+#include "Renderer.h"
---
+++ scripts/Graphics/Renderer.h
@@ -0,0 +1,16 @@
+//
+// Created by karti on 25-05-2024.
+//
+
+#ifndef RENDERER_H
+#define RENDERER_H
+
+
+
+class Renderer {
+
+};
+
+
+
+#endif //RENDERER_H
